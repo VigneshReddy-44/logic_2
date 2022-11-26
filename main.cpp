fc41847f6305d1467605f3c445f7bbd38c2b36df
vigneshreddy <vrtarimala@gmail.com>
2022-11-27 01:01:35 +0530

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,187 @@
+#include<iostream>
+#include<chrono>
+#include<string>
+using namespace std;
+using namespace std::chrono;
+
+bool andIntroduction(string* proof_lines,string currentLine,int currentLineNumber,int line1Ref,int line2Ref){
+    if(line1Ref>=currentLineNumber || line2Ref>=currentLineNumber){
+        return false;
+    }
+    string line1Statement=proof_lines[line1Ref-1].substr(0,proof_lines[line1Ref-1].find("/"));
+    string line2Statement=proof_lines[line2Ref-1].substr(0,proof_lines[line2Ref-1].find("/"));
+    string expectedStatement="(";
+    expectedStatement+=line1Statement+"^"+line2Statement+")";
+    if(expectedStatement==currentLine){
+        return true;
+    }else{
+        return false;
+    }
+
+}
+bool implication_elimination(string* proof_lines,string currentLine,int currentLineNumber,int line1Ref,int line2Ref){
+    if(line1Ref>=currentLineNumber || line2Ref>=currentLineNumber){
+        return false;
+    }
+    string line1Statement=proof_lines[line1Ref-1].substr(0,proof_lines[line1Ref-1].find("/"));
+    string line2Statement=proof_lines[line2Ref-1].substr(0,proof_lines[line2Ref-1].find("/"));
+    string expectedConclusion=line1Statement.substr(line1Statement.find(">")+1,line1Statement.length()-1-line1Statement.find(">")-1);
+    string hypothesis=line1Statement.substr(1,line1Statement.find(">")-1);
+    if(hypothesis==line2Statement && expectedConclusion==currentLine){
+        return true;
+    }else{
+        return false;
+    }
+}
+bool orIntroduction1(string* proof_lines,string currentLine,int currentLineNumber,int line1Ref){
+    if(line1Ref>=currentLineNumber){
+        return false;
+    }
+    string line1Statement=proof_lines[line1Ref-1].substr(0,proof_lines[line1Ref-1].find("/"));
+    string expectedOutputPart="("+line1Statement+"+";
+    int l=currentLine.find(expectedOutputPart);
+    if(l>=0 && currentLine.length()>expectedOutputPart.length()+1){
+        return true;
+    }else{
+        return false;
+    }
+}
+bool orIntroduction2(string* proof_lines,string currentLine,int currentLineNumber,int line1Ref){
+    if(line1Ref>=currentLineNumber){
+        return false;
+    }
+    string line1Statement=proof_lines[line1Ref-1].substr(0,proof_lines[line1Ref-1].find("/"));
+    string expectedOutputPart="+"+line1Statement+")";
+    int l=currentLine.find(expectedOutputPart);
+    if(l>=2 && currentLine.length()>expectedOutputPart.length()+1){
+        return true;
+    }else{
+        return false;
+    }
+}
+bool andElimination1(string* proof_lines,string currentLine,int currentLineNumber,int line1Ref){
+    if(line1Ref>=currentLineNumber){
+        return false;
+    }
+    string line1Statement=proof_lines[line1Ref-1].substr(0,proof_lines[line1Ref-1].find("/"));
+    string expectedOutput=line1Statement.substr(1,line1Statement.find("^")-1);
+    if(expectedOutput==currentLine){
+        return true;
+    }else{
+        return false;
+    }
+}
+bool andElimination2(string* proof_lines,string currentLine,int currentLineNumber,int line1Ref){
+    if(line1Ref>=currentLineNumber){
+        return false;
+    }
+    string line1Statement=proof_lines[line1Ref-1].substr(0,proof_lines[line1Ref-1].find("/"));
+    string expectedOutput=line1Statement.substr(line1Statement.find("^")+1);
+    if(expectedOutput==currentLine){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+
+
+bool proof_Evaluator(string* proof_lines,int number_of_lines){
+
+    for(int i=0;i<number_of_lines;i++){
+
+        int line1,line2;
+
+        string line=proof_lines[i];
+        string ruleTemp=line.substr(line.find("/")+1);
+        string statement=line.substr(0,line.find("/"));
+        //string line_ref=ruleTemp.substr()
+        if(ruleTemp.length()>1){
+            string rule=ruleTemp.substr(0,ruleTemp.find("/"));
+        }
+
+        if(ruleTemp=="P"){
+            continue;
+        }
+        string rule=ruleTemp.substr(0,ruleTemp.find("/"));
+        ruleTemp=ruleTemp.substr(ruleTemp.find(rule)+rule.length()+1);
+        if(rule=="^i" || rule==">e"){
+            string lineRef1=ruleTemp.substr(0,ruleTemp.find("/"));
+            string lineRef2=ruleTemp.substr(ruleTemp.find("/")+1);
+            line1= stoi(lineRef1);
+            line2= stoi(lineRef2);
+            if(rule=="^i"){
+                bool b= andIntroduction(proof_lines,statement,i+1,line1,line2);
+                if(b==false){
+                    return false;
+                }
+            }
+            if(rule==">e"){
+                bool b=implication_elimination(proof_lines,statement,i+1,line1,line2);
+                if(b==false){
+                    return false;
+                }
+            }
+        }else{
+            string lineRef1=ruleTemp.substr(0,ruleTemp.find("/"));
+            line1= stoi(lineRef1);
+            if(rule=="+i1"){
+                bool b= orIntroduction1(proof_lines,statement,i+1,line1);
+                if(b==false){
+                    return false;
+                }
+            }else if(rule=="+i2"){
+                bool b= orIntroduction2(proof_lines,statement,i+1,line1);
+                if(b==false){
+                    return false;
+                }
+
+            }else if(rule=="^e1"){
+                bool b= andElimination1(proof_lines,statement,i+1,line1);
+                if(b==false){
+                    return false;
+                }
+
+            }else if(rule=="^e2"){
+                bool b= andElimination2(proof_lines,statement,i+1,line1);
+                if(b==false){
+                    return false;
+                }
+            }
+
+        }
+
+
+
+
+
+
+    }
+
+
+
+    return true;
+}
+
+int main() {
+    int number_of_lines;
+    cin>>number_of_lines;
+    string* proof_lines=new string[number_of_lines];
+    for(int i=0;i<number_of_lines;i++){
+        cin>>proof_lines[i];
+    }
+    cout<<endl;
+    auto start = high_resolution_clock::now();
+    bool validity= proof_Evaluator(proof_lines,number_of_lines);
+    auto stop = high_resolution_clock::now();
+    if(validity== true){
+        cout<<"Valid Proof"<<endl;
+    }else{
+        cout<<"Invalid proof"<<endl;
+    }
+    cout<<endl;
+    auto duration = duration_cast<microseconds>(stop - start);
+    cout<<"Execution time: "<<duration.count()<<"\xC2\xB5"<<endl;
+
+
+}
